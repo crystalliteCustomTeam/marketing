1520e57534c3faf591b2e4830f41675c59b8faa8
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-10-10 12:30:27 -0700

    fixes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,9 +14,9 @@ export default function Page() {
       <Publishing />
       <Sessions />
       <PublishedBooks />
+      <Contact />
       <LatestPublished/>
       <Interviews/>
-      <Contact />
       <WhyChoose />
       <Billboard />
       <Reviews />
--- src/components/website/home/contact/contact.jsx
+++ src/components/website/home/contact/contact.jsx
@@ -11,7 +11,7 @@ export default function contact() {
     return (
         <section>
             <div className={style.root}>
-                <Image src={Book} alt="domain" className="lg:absolute lg:right-[50px] 2xl:right-[100px] 3xl:right-[50px] lg:bottom-[-50px] 2xl:bottom-[-100px] lg:w-[50%]" />
+                <Image src={Book} alt="domain" className="lg:absolute lg:right-[50px] 2xl:right-[100px] 3xl:right-[50px] lg:bottom-[-50px]  lg:w-[50%]" />
                 <div className="container">
                     <div className="lg:w-[55%] xl:w-[50%] 3xl:w-[55%] mt-10 lg:mt-0">
                         <h2 className={style.title}>We would love to hear from you!</h2>
