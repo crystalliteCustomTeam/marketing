6b6657656d2798b4c4ada81885c8b5b090721a3e
dev-mr <m.rizwan@crystal-lite.net>
2024-10-26 01:00:48 +0500

    Fix Css

--- src/components/website/reviews/cards/cards.jsx
+++ src/components/website/reviews/cards/cards.jsx
@@ -15,6 +15,48 @@ import { CTAGroupReviews, Fancybox, NextButton, PrevButton, usePrevNextButtons }
 // Next
 import Image from "next/image"
 
+const data1 = [
+    [Client1],
+    [Client2],
+    [Client3],
+    [Client1],
+    [Client2],
+    [Client3]
+]
+
+const data2 = [
+    [
+        "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
+        "Amber Wanner",
+        "CEO, Vette, Inc."
+    ],
+    [
+        "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
+        "Amber Wanner",
+        "CEO, Vette, Inc."
+    ],
+    [
+        "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
+        "Amber Wanner",
+        "CEO, Vette, Inc."
+    ],
+    [
+        "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
+        "Amber Wanner",
+        "CEO, Vette, Inc."
+    ],
+    [
+        "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
+        "Amber Wanner",
+        "CEO, Vette, Inc."
+    ],
+    [
+        "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
+        "Amber Wanner",
+        "CEO, Vette, Inc."
+    ],
+]
+
 export default function Testimonials() {
     const [selectedIndex, setSelectedIndex] = useState(0)
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: "center", })
@@ -59,14 +101,7 @@ export default function Testimonials() {
                 className="flex touch-pan-y ml-[calc(1rem_*_-1)]"
             >
                 {
-                    [
-                        [Client1],
-                        [Client2],
-                        [Client3],
-                        [Client1],
-                        [Client2],
-                        [Client3]
-                    ].map(([img], i) => (
+                    data1.map(([img], i) => (
                         <div key={i} className={styles.embla__slide}>
                             <a data-fancybox="gallery" href="https://vimeo.com/1015023376">
                                 <Image src={img} alt="domain" placeholder="blur" />
@@ -79,38 +114,7 @@ export default function Testimonials() {
             <div className={styles.embla_thumbs} ref={emblaThumbsRef}>
                 <div className={styles.embla_thumbs__container}>
                     {
-                        [
-                            [
-                                "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
-                                "Amber Wanner",
-                                "CEO, Vette, Inc."
-                            ],
-                            [
-                                "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
-                                "Amber Wanner",
-                                "CEO, Vette, Inc."
-                            ],
-                            [
-                                "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
-                                "Amber Wanner",
-                                "CEO, Vette, Inc."
-                            ],
-                            [
-                                "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
-                                "Amber Wanner",
-                                "CEO, Vette, Inc."
-                            ],
-                            [
-                                "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
-                                "Amber Wanner",
-                                "CEO, Vette, Inc."
-                            ],
-                            [
-                                "“We’re impressed with their entrepreneurial approach to handling their projects. They really put the process in a way that I can understand and help me focus on my responsibilities as a CEO.”",
-                                "Amber Wanner",
-                                "CEO, Vette, Inc."
-                            ],
-                        ].map(([desc, name, position], i) => (
+                        data2.map(([desc, name, position], i) => (
                             <div
                                 key={i}
                                 className={`${styles.embla_thumbs__slide}`}
