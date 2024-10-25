b1a23d2ea64ddbc5184a8adfa11317fc2d9ca87f
dev-mr <m.rizwan@crystal-lite.net>
2024-10-26 04:39:09 +0500

    Fix Home Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,23 @@
-// Components
-import { Awards, WhyChoose, Reviews, TrustedClients } from "@/components"
-import { Hero, Publishing, Services, Sessions, TrendingPodcast, Billboard, PublishedBooks, Interviews, LatestPublished, Contact } from "@/components/website/home"
+import React, { Suspense, lazy } from "react"
+
+const Hero = lazy(() => import("@/components/website/home/hero/hero"))
+const Awards = lazy(() => import("@/components/website/awards/awards"))
+const Services = lazy(() => import("@/components/website/home/services/services"))
+const TrendingPodcast = lazy(() => import("@/components/website/home/trending-podcast/trending-podcast"))
+const Publishing = lazy(() => import("@/components/website/home/publishing/publishing"))
+const Sessions = lazy(() => import("@/components/website/home/sessions/sessions"))
+const PublishedBooks = lazy(() => import("@/components/website/home/published-books/published-books"))
+const Contact = lazy(() => import("@/components/website/home/contact/contact"))
+const LatestPublished = lazy(() => import("@/components/website/home/latest-published/latest-published"))
+const Interviews = lazy(() => import("@/components/website/home/interviews/interviews"))
+const WhyChoose = lazy(() => import("@/components/website/why-choose/why-choose"))
+const Billboard = lazy(() => import("@/components/website/home/billborad/billboard"))
+const Reviews = lazy(() => import("@/components/website/reviews/reviews"))
+const TrustedClients = lazy(() => import("@/components/website/trusted-clients/trusted-clients"))
 
 export default function Page() {
-
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Hero />
       <Awards />
       <Services />
@@ -20,6 +32,6 @@ export default function Page() {
       <Billboard />
       <Reviews />
       <TrustedClients />
-    </>
+    </Suspense>
   )
 }
